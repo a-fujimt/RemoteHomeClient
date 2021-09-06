ee7f17df8dc89b6ac0165b9ccbd66a0a0b70e38d
a-fujimt <a-fujimt@ist.osaka-u.ac.jp>
2021-09-06 12:41:39 +0900

    Initial Commit

---
+++ Smarthome/SmarthomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmarthomeApp.swift
+//  Smarthome
+//
+//  Created by 藤本 章良 on 2021/09/06.
+//
+
+import SwiftUI
+
+@main
+struct SmarthomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
